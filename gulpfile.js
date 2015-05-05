a974ccaac64d2f4f949ebf0fefbc903677f5748a
gaspard <gaspard@gmail.com>
2015-05-05 15:31:37 +0200

    basic webs structure

--- gulpfile.js
+++ gulpfile.js
@@ -3,49 +3,75 @@ var gutil = require('gulp-util');
 var bower = require('gulp-bower');
 var connect = require('gulp-connect');
 var less = require('gulp-less');
+var jasmine = require('gulp-jasmine');
 
-
-gulp.task('less', function () {
-  gulp.src(['./src/less/app.less', './src/less/bootstrap.less'])
-    .pipe(less()) // should use paths: [ path.join(__dirname, 'src', 'includes') ]
-    .pipe(gulp.dest('./build/css'))
-    .pipe(connect.reload());
+gulp.task('tests', function () {
+  return gulp.src('tests/*.js')
+    .pipe(jasmine());
 });
 
-
-gulp.task('watch', function () {
-  // gulp.watch(['./src/**.html'], ['html']);
-  gulp.watch(['./src/less/*.less'], ['less']);
+gulp.task('bower', function() {
+  return bower();
 });
 
+gulp.task('install', ['bower'], function() {
+  var bc = 'bower_components',
+    files = [
+      bc + '/jquery/dist/jquery.js',
+      bc + '/bootstrap/dist/js/bootstrap.js',
+      bc + '/jssip/dist/jssip.js'
+    ];
 
-gulp.task('serve', ['watch'], function () {
+  return gulp.src(files)
+    .pipe(gulp.dest('./dist/js/'));
+});
+
+gulp.task('serve', ['default', 'watch'], function () {
   connect.server({
-      root: ['./src', './build'],
+      port: process.env.PORT || 8080,
+      root: './dist',
       livereload: true
     });
 });
 
-gulp.task('jslib', function() {
-  gulp.src([
-    './bower_components/bootstrap/dist/js/bootstrap.js',
-    './bower_components/jquery/dist/jquery.js',
-    './bower_components/underscore/underscore.js',
-    './bower_components/highlight/src/highlight.js',
-  ]).pipe(gulp.dest('./build/js/lib/'));
+gulp.task('html', function () {
+  console.log("html updated");
+  gulp.src('./src/**.html')
+    .pipe(gulp.dest('./dist/'))
+    .pipe(connect.reload());
+});
+
+gulp.task('less', function () {
+  gulp.src(['./src/less/app.less', './src/less/bootstrap.less'])
+    .pipe(less()) // should use paths: [ path.join(__dirname, 'src', 'includes') ]
+    .pipe(gulp.dest('./dist/css'))
+    .pipe(connect.reload());
+});
+
+gulp.task('js', function () {
+  gulp.src('./src/js/*.js')
+    .pipe(gulp.dest('./dist/js'))
+    .pipe(connect.reload());
 });
 
 gulp.task('images', function () {
-  gulp.src(['./src/images/**'])
-  .pipe(gulp.dest('./build/images/'));
+  gulp.src('./src/images/*')
+    .pipe(gulp.dest('./dist/images'))
+    .pipe(connect.reload());
 });
 
-gulp.task('fonts', function () {
-  gulp.src(['./bower_components/bootstrap/dist/fonts/**'])
-  .pipe(gulp.dest('./build/fonts/'));
+gulp.task('favicon', function() {
+  gulp.src('./src/favicon.ico')
+    .pipe(gulp.dest('./dist'));
 });
 
-gulp.task('build', ['images', 'fonts'], function() {
-  gulp.src('./src/*.html')
-    .pipe(gulp.dest('./build/'));
-})
+gulp.task('watch', function () {
+  gulp.watch(['./src/**.html'], ['html']);
+  gulp.watch(['./src/less/*.less'], ['less']);
+  gulp.watch(['./src/js/*.js'], ['js']);
+  gulp.watch(['./src/images/*'], ['images'])
+});
+
+gulp.task('default', ['less', 'html', 'js', 'images'], function(){
+
+});
