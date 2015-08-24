9cf37a838e716fbb34657f8f1000057623df84c6
gaspard <gaspard@gmail.com>
2015-08-24 12:57:39 +0200

    utils: moved to a separate file

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,4 @@
-'use strict';
+// general app
 
 var config = {};
 
@@ -20,39 +20,6 @@ config.constraints = {
       maxWidth: 320
     }
   }
-};
-
-var utils = {
-  slugify: function (text) {
-    return text.toString().toLowerCase()
-      .replace(/\s+/g, '-')           // Replace spaces with -
-      .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
-      .replace(/\-\-+/g, '-')         // Replace multiple - with single -
-      .replace(/^-+/, '')             // Trim - from start of text
-      .replace(/-+$/, '');            // Trim - from end of text
-  },
-  yaddahyaddah: function (len, charSet) {
-    len = len || 16;
-    charSet = charSet || 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    var randomString = '';
-    for (var i = 0; i < len; i++) {
-      var randomPoz = Math.floor(Math.random() * charSet.length);
-      randomString += charSet.substring(randomPoz, randomPoz + 1);
-    }
-    return randomString;
-  }
-};
-
-
-var debug = function(message, e) {
-  if(typeof(e)==='undefined') {
-    e = null;
-  }
-  else {
-    console.log(message, e);
-  }
-  var $l = document.getElementById('debug');
-  $l.innerHTML = $l.innerHTML + message +  ' \n';
 };
 
 // fire !
